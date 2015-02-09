2ad88bc89fa8ffb668c239216c02b4dc7b69fcc3
Antti Nieminen <anttihn@gmail.com>
2015-02-09 14:25:27 +0200

    Added Sequelize.
    A bit like this:
    http://sequelizejs.com/articles/express

---
+++ models/user.js
@@ -0,0 +1,19 @@
+"use strict";
+
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define("User", {
+    username: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // Tässä voi assosioida malleja toisiinsa
+        // http://sequelize.readthedocs.org/en/latest/docs/associations/
+        //
+        // Tyyliin
+        // User.hasMany(models.BlogPost);
+      }
+    }
+  });
+
+  return User;
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var models = require('../models');
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  models.User.findAll().then(function(users) {
+    res.render('index', {
+      title: 'Express',
+      users: users
+    });
+  });
 });
 
 module.exports = router;
