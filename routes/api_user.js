f7382e85171043dbb17ef94843679f48801824e4
Antti Nieminen <anttihn@gmail.com>
2015-02-18 09:50:38 +0200

    Removed unused function.

--- routes/api_user.js
+++ routes/api_user.js
@@ -3,10 +3,6 @@ var router = express.Router();
 
 var models = require('../models');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 
 router.post('/', function(req, res, next) {
 
