cb2a9327e382d243a0541d7c4ed4f1ab43e78564
Noora Männikkö <noora.mannikko@student.tut.fi>
2015-03-15 13:54:00 +0200

    Huono kirjautuminen

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 var models = require('../models');
 
+var passport = require('passport');
+var BasicStrategy = require('passport-http').Strategy;
+
 router.get('/', function(req, res, next) {
   models.User.findAll().then(function(users) {
     res.render('index', {
@@ -11,5 +14,28 @@ router.get('/', function(req, res, next) {
     });
   });
 });
+
+/*router.post('/login', function(req, res, next) {
+	var username = req.body.username;
+	var query = {where: {username: username}};
+	// Etsi käyttäjä tunnuksella
+  	models.User.findOne(query).then(function(user) {
+  		if (user) {
+  			// Tarkista salasana
+  			var password = req.body.password;
+  			if (password === user.password) {
+  				// Tallenna kirjautuminen
+  				// Väliaikainen ratkaisu:
+  				return res.status(200).json();
+  			}
+  			else {
+  				return res.status(404).json({error: 'InvalidPassword'});
+  			}
+  		}
+  		else {
+  			return res.status(404).json({error: 'UserNotFound'});
+  		}
+  	});
+});*/
 
 module.exports = router;
